30d8c16e0f545aefa6b7f537e076169d89483d2d
Aledebaran <byronaldebaran2@hotmail.com>
2025-01-02 14:07:03 -0600

    cambios

--- api/BD/Accesos_BD.ts
+++ api/BD/Accesos_BD.ts
@@ -11,11 +11,22 @@ import mysql from 'mysql2/promise';
 // }
 
 
+// export async function connect() {
+//     return mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'systemab_funeraria_db',
+//         timezone: 'local',
+//     });
+// }
+
+
 export async function connect() {
     return mysql.createConnection({
-        host: 'localhost',
+        host: '66.179.189.37',
         user: 'root',
-        password: '',
+        password: 'wWi8aiDa',
         database: 'systemab_funeraria_db',
         timezone: 'local',
     });
