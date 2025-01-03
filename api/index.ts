216e53ef0f35999936bb8263c6557de4a4474bfa
Aledebaran <byronaldebaran2@hotmail.com>
2025-01-03 00:26:23 -0600

    cambios

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { App } from './app';
 
 async function main() {
-    const app = new App(443);
+    const app = new App(3309);
     await app.listen();
 }
 
--- cliente/FunerariaApp-main/src/environments/environment.prod.ts
+++ cliente/FunerariaApp-main/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environmentProd = {
   production: true,
-  endpoint: 'https://systemabmxlifuneraria.com',  // Nota el '/api' después del puerto
+  endpoint: 'https://systemabmxlifuneraria.com/',  // Nota el '/api' después del puerto
 };
