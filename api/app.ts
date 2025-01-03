3d1ef0a01d5b07c5f662edd27fb228b6f865f596
Aledebaran <byronaldebaran2@hotmail.com>
2025-01-03 01:34:08 -0600

    regreso a como estaba

--- api/app.ts
+++ api/app.ts
@@ -3,20 +3,24 @@ import express, { Application } from 'express';
 import morgan from 'morgan';
 import path from 'path';
 
-// Importar rutas
-import Usuarios from './routes/Usuarios.routes';
-import Ingresos from './routes/Ingresos.routes';
-import Reconciliaciones from './routes/Reconciliaciones.routes';
-import Combinaciones from './routes/Combinaciones.routes';
-import Notificaciones from './routes/Notificaciones.routes';
-import ImportarIngresos from './routes/Importar-Ingresos.routes';
-import ImportarIngresosArchivo from './routes/Importar-Ingresos-Archivo.routes';
-import Proveedores from './routes/Proveedores.routes';
-import Presupuesto from './routes/Presupuesto.routes';
-import PresupuestoMensualFrecuencia from './routes/Presupuesto-Mensual-Frecuencia.routes';
-import PresupuestoMensualCuentas from './routes/Presupuesto-Mensual-Cuentas.routes';
-import Transferencias from './routes/Transferencias.routes';
-import Proyeccion from './routes/Proyeccion.routes';
+import Usuarios from "./routes/Usuarios.routes"
+import Ingresos from "./routes/Ingresos.routes"
+import Reconciliaciones from "./routes/Reconciliaciones.routes"
+import Combinaciones from "./routes/Combinaciones.routes"
+import Notificaciones from "./routes/Notificaciones.routes"
+import ImportarIngresos from "./routes/Importar-Ingresos.routes"
+import ImportarIngresosArchivo from "./routes/Importar-Ingresos-Archivo.routes"
+import Proveedores from "./routes/Proveedores.routes"
+import Presupuesto from "./routes/Presupuesto.routes"
+import PresupuestoMensualFrecuencia from "./routes/Presupuesto-Mensual-Frecuencia.routes"
+import PresupuestoMensualCuentas from "./routes/Presupuesto-Mensual-Cuentas.routes"
+import Transferencias from "./routes/Transferencias.routes"
+import Proyeccion from "./routes/Proyeccion.routes"
+
+
+
+const dir = '../../cliente/DirectoriCliente/';
+
 
 export class App {
     private app: Application;
@@ -28,26 +32,36 @@ export class App {
         this.routes();
     }
 
-    private settings() {
+    settings() {
         this.app.set('port', this.port || process.env.PORT || 3000);
+        this.app.set('path', dir);
     }
 
-    private middlewares() {
+    allowCrossDomain(req: any, res: any, next: any) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Authorization, Content-Length');
+        if ('OPTIONS' == req.method) res.send(200);
+        else next();
+    }
+
+    middlewares() {
         this.app.use(morgan('dev'));
         this.app.use(cors({
-            origin: '*', // Cambia '*' al dominio de tu cliente en producción
-            methods: ['GET', 'POST', 'PUT', 'DELETE'],
-            credentials: true
+            origin: '*', // Asegúrate de cambiar esto al dominio de tu cliente
+            methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+            credentials: true // Si necesitas enviar cookies
         }));
+        this.app.use(this.allowCrossDomain);
         this.app.use(express.json({ limit: '1mb' }));
         this.app.use(express.urlencoded({ extended: true }));
-
-        // Configuración para servir archivos estáticos
-        this.app.use(express.static(path.resolve(__dirname, '../../cliente/DirectoriCliente')));
+        // this.app.use('/api/uploads', express.static(path.join(__dirname, '../uploads')));
+        this.app.use('/uploads', express.static(path.join(__dirname, '../../uploads/')));
     }
 
-    private routes() {
-        // Registrar rutas
+
+    routes() {
+       
         this.app.use('/api/usuarios', Usuarios);
         this.app.use('/api/ingresos', Ingresos);
         this.app.use('/api/reconciliaciones', Reconciliaciones);
@@ -62,15 +76,15 @@ export class App {
         this.app.use('/api/transferencias', Transferencias);
         this.app.use('/api/proyeccion', Proyeccion);
 
-        // Fallback para aplicaciones SPA
-        this.app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, '../../cliente/DirectoriCliente'));
+
+        this.app.get('*',function (req,res){
+            res.sendfile(path.join(dir, 'index.html'));
         });
     }
 
+
     async listen() {
-        this.app.listen(this.app.get('port'), () => {
-            console.log(`Server on port: ${this.app.get('port')}`);
-        });
+        this.app.listen(this.app.get('port'));
+        console.log('server on port:', this.app.get('port'));
     }
 }
