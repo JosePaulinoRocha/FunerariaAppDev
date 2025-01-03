0dcfc76af97ac5e4507bfc80edc9c64cf529d3dc
Aledebaran <byronaldebaran2@hotmail.com>
2025-01-03 01:50:03 -0600

    actualizacion de cadena de conexion

--- api/BD/Accesos_BD.ts
+++ api/BD/Accesos_BD.ts
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 export async function connect() {
     return mysql.createConnection({
         host: 'localhost',  // Dirección del servidor MySQL
-        port: 3000,  // Puerto predeterminado de MySQL
+        port: 3306,  // Puerto predeterminado de MySQL
         user: 'root',  // Usuario de MySQL
-        password: 'doflamingo011018',  // Contraseña del usuario
+        password: 'wWi8aiDa',  // Contraseña del usuario
         database: 'systemab_funeraria_db',  // Nombre de la base de datos
         timezone: 'local',  // Configuración de zona horaria
     });
