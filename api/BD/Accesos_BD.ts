8fc4d8c3253196498ff5c57c60625239ec3b3e2a
Aledebaran <byronaldebaran2@hotmail.com>
2025-01-03 01:53:27 -0600

    error en cadena

--- api/BD/Accesos_BD.ts
+++ api/BD/Accesos_BD.ts
@@ -5,7 +5,7 @@ export async function connect() {
         host: 'localhost',  // Dirección del servidor MySQL
         port: 3306,  // Puerto predeterminado de MySQL
         user: 'root',  // Usuario de MySQL
-        password: 'wWi8aiDa',  // Contraseña del usuario
+        password: 'doflamingo011018',  // Contraseña del usuario
         database: 'systemab_funeraria_db',  // Nombre de la base de datos
         timezone: 'local',  // Configuración de zona horaria
     });
