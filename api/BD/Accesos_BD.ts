54e7b7fb32a183da9216fca93fe830cee31d8755
Jose Paulino Rocha Martinez <josepaulinorocha890@gmail.com>
2025-01-14 13:53:56 -0600

    merge de cambios

--- api/BD/Accesos_BD.ts
+++ api/BD/Accesos_BD.ts
@@ -1,24 +1,24 @@
 import mysql from 'mysql2/promise';
 
-// export async function connect() {
-//     return mysql.createConnection({
-//         host: 'systemabmxlifuneraria.com',
-//         port: 3306, 
-//         user: 'root',  
-//         password: 'doflamingo011018',
-//         database: 'systemab_funeraria_db', 
-//         timezone: 'local', 
-//     });
-// }
-
-
-
 export async function connect() {
     return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'systemab_funeraria_db',
-        timezone: 'local',
+        host: 'systemabmxlifuneraria.com',
+        port: 3306, 
+        user: 'root',  
+        password: 'doflamingo011018',
+        database: 'systemab_funeraria_db', 
+        timezone: 'local', 
     });
 }
+
+
+
+// export async function connect() {
+//     return mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'systemab_funeraria_db',
+//         timezone: 'local',
+//     });
+// }
